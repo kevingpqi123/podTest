04a9c1d816f293612db77099dacaf1abdfa47e00
kevingpqi <kevingpqi@tencent.com>
2021-01-20 14:32:29 +0800

    1.0.4

--- Example/Pods/Target Support Files/Pods-podTest_Example/Pods-podTest_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-podTest_Example/Pods-podTest_Example-frameworks.sh
@@ -175,9 +175,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${PODS_ROOT}/../../framework/podTest.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/podTest/podTest.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${PODS_ROOT}/../../framework/podTest.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/podTest/podTest.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
